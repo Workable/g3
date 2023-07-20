bbd75dfc0d41a0159ca55acc655846ec9ad91e62
rousopoulosk <konstantinos.rousopoulos@workable.com>
2023-07-20 19:42:45 +0300

    properly split git by protocol

--- g3/git/git.py
+++ g3/git/git.py
@@ -1,9 +1,27 @@
+from enum import Enum
 from typing import List, Optional
 
 from pydantic import BaseModel
 
 from g3.git.diff import Diff, get_filenames, get_files_changed
 from g3.git.shell import Shell
+
+
+# Make an enum with git protocol options
+class GitProtocol(str, Enum):
+    HTTPS = "https"
+    SSH = "git"
+
+
+def parse_git_remote_info(repo_info: str) -> List[str]:
+    if repo_info.startswith(GitProtocol.HTTPS.value):
+        result = repo_info.split("/")
+        return [result[-1].removesuffix(".git"), result[-2]]
+    elif repo_info.startswith(GitProtocol.SSH.value):
+        result = repo_info.split(":")[1].split("/")
+        return [result[-1].removesuffix(".git"), result[-2]]
+    else:
+        raise ValueError("Git protocol not supported")
 
 
 class ModelData(BaseModel):
@@ -15,7 +33,7 @@ class ModelData(BaseModel):
 
 class GitInfo(BaseModel):
     repo_owner: Optional[str] = None
-    repo: Optional[str] = None
+    repo_name: Optional[str] = None
     branch: Optional[str] = None
 
     def __init__(self, **kwargs):
@@ -27,9 +45,8 @@ class GitInfo(BaseModel):
         assert sh.is_git()
 
         self.branch = sh.git("rev-parse", "--abbrev-ref", "HEAD")
-        repo_info = sh.git("config", "--get", "remote.origin.url").split("/")
-        self.repo = repo_info[-1]
-        self.repo_owner = repo_info[-2]
+        repo_info = sh.git("config", "--get", "remote.origin.url")
+        self.repo_name, self.repo_owner = parse_git_remote_info(repo_info)
 
     def get_data_for_model(self) -> ModelData:
         sh = Shell()
