183e0f37de303b049d3c022843031412e0a43380
rousopoulosk <konstantinos.rousopoulos@workable.com>
2023-07-20 18:35:29 +0300

    adds excluded files

--- g3/git/diff.py
+++ g3/git/diff.py
@@ -2,6 +2,7 @@ from typing import Generator, List
 
 from pydantic import BaseModel
 
+from g3.config.exclude import EXCLUDED_FILES
 from g3.git.shell import Shell
 
 
@@ -16,6 +17,11 @@ def get_filenames(sh: Shell) -> List[str]:
 
 def get_files_changed(sh: Shell) -> Generator[Diff, None, None]:
     diffs = get_filenames(sh)
+    diffs = exclude_files(diffs)
     for diff in diffs:
         patch = sh.git("diff", "HEAD", "--function-context", diff)
         yield Diff(filename=diff, patch=patch)
+
+
+def exclude_files(files: List[str]) -> List[str]:
+    return list(filter(lambda x: x not in EXCLUDED_FILES, files))
--- tests/git/test_diff.py
+++ tests/git/test_diff.py
@@ -1,2 +1,11 @@
+from g3.git.diff import exclude_files
+
+
+def test_exclude_files():
+    files = ["test.txt", "package-lock.json", "poetry.lock", "Gemfile.lock"]
+
+    assert exclude_files(files) == ["test.txt"]
+
+
 def test_get_files_changed():
     pass
