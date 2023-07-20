059ca967624e8593f8253c694fb4c39e996be9f6
Petros Lamb <petros.lambropoulos@workable.com>
2023-07-20 20:12:29 +0300

    Add token count for diffs with tiktoken

--- g3/git/data.py
+++ g3/git/data.py
@@ -1,9 +1,12 @@
-from typing import List
+from typing import List, Optional
+
+import tiktoken
 
 from pydantic import BaseModel
 
 from g3.git.diff import Diff, get_filenames, get_files_changed
 from g3.git.shell import Shell
+from g3.config import config
 
 
 class ModelData(BaseModel):
@@ -11,6 +14,14 @@ class ModelData(BaseModel):
     diffs: List[Diff]
     repo_name: str
     branch_name: str
+
+    @property
+    def tokens_of_diffs(self) -> Optional[int]:
+        if not self.diffs:
+          return None
+
+        encoding = tiktoken.encoding_for_model(config.model)
+        return sum(len(encoding.encode(diff)) for diff in self.diffs)
 
 
 def get_data_for_model() -> ModelData:
