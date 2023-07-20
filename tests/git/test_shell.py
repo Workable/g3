265dc7c0a38843d49608cc87c3ef8ae5990aa11a
rousopoulosk <konstantinos.rousopoulos@workable.com>
2023-07-20 13:10:16 +0300

    shell for abstracting git commands

---
+++ tests/git/test_shell.py
@@ -0,0 +1,13 @@
+import os
+
+from g3.git.shell import Shell
+
+
+def test_shell():
+    sh = Shell()
+    assert sh.cwd == os.getcwd()
+
+    assert sh.is_git()
+
+    repo_name = sh.repo_name()
+    assert repo_name == "g3"
