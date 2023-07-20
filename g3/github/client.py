166a305052c33b9ff0d83f451f8708a7ae27c608
Panos Antonakos <42936184+antonakospanos@users.noreply.github.com>
2023-07-20 21:39:13 +0300

    ADD: GH client

---
+++ g3/github/client.py
@@ -0,0 +1,20 @@
+from github import Github
+
+from g3.config import config
+from g3.git.git import GitInfo
+
+
+class Client:
+    def __init__(self):
+        self.github_token = config.github_token
+        self.g = Github(self.github_token)
+        git_info = GitInfo()
+        self.repo = self.g.get_repo(f"{git_info.repo_owner}/{git_info.repo}")
+        self.head = git_info.branch
+
+    def open_pull_request(self, title, body, base=None):
+        if base is None:
+            base = self.repo.default_branch
+
+        pr = self.repo.create_pull(title=title, body=body, head=self.head, base=base)
+        return pr
