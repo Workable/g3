ebebf261f64f0d0d7353463c43bc5ed01d94d6bf
Ioannis Karachristos <ioannis@workable.com>
2023-07-21 12:17:50 +0300

    Add --edit functionality for past commits

--- g3/github/client.py
+++ g3/github/client.py
@@ -1,4 +1,7 @@
-from github import Github
+from typing import Optional
+
+from github import Auth, Github
+from github.Commit import Commit
 from github.PullRequest import PullRequest
 
 from g3.config import config
@@ -8,10 +11,13 @@ from g3.git.gitinfo import GitInfo
 class Client:
     def __init__(self):
         self.github_token = config.github_token
-        self.g = Github(self.github_token)
+        self.g = Github(auth=Auth.Token(self.github_token))
         git_info = GitInfo()
         self.repo = self.g.get_repo(f"{git_info.repo_owner}/{git_info.repo}")
         self.head = git_info.branch
+
+    def get_commit(self, commit_hash: Optional[str] = None) -> Optional[Commit]:
+        return self.repo.get_commit(sha=commit_hash) if commit_hash else None
 
     def open_pull_request(self, title, body, base=None) -> PullRequest:
         if base is None:
