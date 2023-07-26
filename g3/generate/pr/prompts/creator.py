39a075e0ed0a90e3c6ca74beacc04853b3b38ac4
Panos Antonakos <42936184+antonakospanos@users.noreply.github.com>
2023-07-26 13:11:01 +0300

    Refactor commit and PR message generation
    - Updated the message generation logic in `creator.py` for both commit and PR messages.
    - Improved the handling of Jira tickets in the message content.
    - Removed unnecessary code and improved readability.
    - These changes enhance the clarity and consistency of generated messages.
    
    PROD-1

--- g3/generate/pr/prompts/creator.py
+++ g3/generate/pr/prompts/creator.py
@@ -2,9 +2,7 @@ from typing import Optional
 
 from g3.domain.message_tone import MessageTone
 from g3.generate.pr.prompts.template import pr_template
-from g3.git.client import get_commit_messages
 from g3.git.gitinfo import GitInfo
-from g3.github.github_info import GithubInfo
 from g3.main import config
 
 
@@ -23,8 +21,7 @@ class Creator:
     @property
     def user_messages(self) -> list:
         content = f"""Please provide a pull request description for the provided commit messages.
- Commits: ```{self.commit_messages}```.
- The commit messages are form a git branch named {self.git_info.branch}.
+ Commits: ```{self.commit_messages}```. The commit messages are form a git branch named {self.git_info.branch}.
  The commit messages are from a git repository named {self.git_info.repo}."""
 
         return [
@@ -47,20 +44,15 @@ class Creator:
  Leave an new empty line always after the title. The pull reuqest's description should be under
  {config.pr_description_max_words} words in total and it should be helpful to the reader in order to
  understand the changes made. The reader of the pull request description needs to understand the changes
- in order to approve the pull request or ask for the needed changes. If the commit messages have referenced
- jira tickets, then include them in the pull request description too, but not in the title. Also, if the user
- provides any additional jira tickets, then include them in the pull request description too.
- Put the jira tickets in bullet points using the template provided.
- Use the following tone when creating the pull request
- message: {tone}.
- {pr_template}"""
+ in order to approve the pull request or ask for the needed changes. If Jira tickets are provided then include them in
+ the pull request description. Use the following tone when creating the pull request message: {tone}."""
         if include:
-            content += f" Include in your response the following: ```{include}```."
+            content += f" Include in your response the following: {include}"
         if jira:
-            content += f" Incorporate the following Jira ticket: ```{jira}```."
+            content += f" Include in the end of your response the following Jira ticket: ```{jira}```."
         content += (
-            "Your response should be in Github markdown syntax format and you should split each "
-            "sentence to a new line."
+            f" Your response should be in Github markdown syntax format and comply on the following template:"
+            f"```{pr_template}```."
         )
 
         return [{"role": "system", "content": content.replace("\n", "")}]
